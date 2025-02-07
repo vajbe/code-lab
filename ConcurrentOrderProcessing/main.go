17885d98e3844523cb7d71310f05af3362e1a4b1
Vivek Ajbe <vajbe.tce@gmail.com>
2025-02-07 23:18:09 +0530

    order scehduler changes

--- ConcurrentOrderProcessing/main.go
+++ ConcurrentOrderProcessing/main.go
@@ -11,11 +11,17 @@ func orderScheduler(orderId int) {
 	fmt.Printf("\nDone with order %d", orderId)
 }
 
-func main() {
-	startTime := time.Now()
+func worker(jobId int, jobs <-chan int, result <-chan int) {
 
+}
+
+func main() {
+	numJobs := 5
+	startTime := time.Now()
+	jobs := make(chan int, numJobs)
+	result := make(chan int, numJobs)
 	for i := 0; i < 50; i++ {
-		go orderScheduler(i)
+		go worker(i, jobs, result)
 	}
 	fmt.Printf("\nStarted at %s eneded at %s\n", startTime, time.Now())
 	fmt.Printf("\nElapsed Time %s\n", time.Since(startTime))
