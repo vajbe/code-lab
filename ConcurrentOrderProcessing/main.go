4834b5a4cbcab57c0c25df67278435791de56b39
Vivek Ajbe <vajbe.tce@gmail.com>
2025-02-08 16:25:25 +0530

    Process e commerce order and validate before updating data with mutex

--- ConcurrentOrderProcessing/main.go
+++ ConcurrentOrderProcessing/main.go
@@ -2,28 +2,71 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
 	"time"
 )
 
-func orderScheduler(orderId int) {
-	fmt.Printf("\nProcessing order %d\t", orderId)
-	time.Sleep(100 * time.Microsecond)
-	fmt.Printf("\nDone with order %d", orderId)
+type Order struct {
+	Id     int
+	Item   string
+	Amount int
 }
 
-func worker(jobId int, jobs <-chan int, result <-chan int) {
+const (
+	numWorkers   = 10  // Number of concurrent workers
+	numOrders    = 50  // Total orders to process
+	initialStock = 100 // Initial stock quantity
+)
 
+var lock sync.Mutex
+var stockLevels = map[string]int{
+	"Laptop": initialStock,
+}
+
+var wg sync.WaitGroup
+
+func ProcessOrder(order Order) {
+	defer wg.Done()
+	time.Sleep(1 * time.Second)
+	lock.Lock()
+	if stockLevels[order.Item] >= order.Amount {
+		stockLevels[order.Item] = stockLevels[order.Item] - order.Amount
+		fmt.Printf("\n Processed order: %d Stocks required:%d Stocks left: %d", order.Id, order.Amount, stockLevels[order.Item])
+	} else {
+		fmt.Printf("\n Failed to process order %d no stock available", order.Id)
+	}
+	lock.Unlock()
+}
+
+func worker(orderQueue <-chan Order) {
+	for order := range orderQueue {
+		ProcessOrder(order)
+	}
 }
 
 func main() {
 	numJobs := 5
-	startTime := time.Now()
-	jobs := make(chan int, numJobs)
-	result := make(chan int, numJobs)
-	for i := 0; i < 50; i++ {
-		go worker(i, jobs, result)
+	orderQueue := make(chan Order, numJobs)
+
+	// Start worker pool
+	for i := 0; i < numWorkers; i++ {
+		go worker(orderQueue)
 	}
-	fmt.Printf("\nStarted at %s eneded at %s\n", startTime, time.Now())
-	fmt.Printf("\nElapsed Time %s\n", time.Since(startTime))
+
+	//Generate and Queue order
+
+	for j := 0; j <= numOrders; j++ {
+		order := Order{
+			Id:     j,
+			Item:   "Laptop",
+			Amount: rand.Intn(5) + 1,
+		}
+		wg.Add(1)
+		orderQueue <- order
+	}
+	close(orderQueue)
+	wg.Wait()
+	fmt.Println("\n🏁 All orders processed!")
 
 }
