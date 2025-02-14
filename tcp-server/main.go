dcb27892251926c36b5e284440b5427ec37af51f
Vivek Ajbe <vajbe.tce@gmail.com>
2025-02-14 11:02:09 +0530

    adding parallel processing

--- tcp-server/main.go
+++ tcp-server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
 var templateResponse = "HTTP/1.1 200 OK\r\n" +
@@ -14,9 +15,12 @@ var templateResponse = "HTTP/1.1 200 OK\r\n" +
 
 func do(conn net.Conn) {
 	buffer := make([]byte, 1024)
+	fmt.Println("Processing...")
+	time.Sleep(8 * time.Second)
 	conn.Read(buffer)
 	conn.Write([]byte(templateResponse))
 	conn.Close()
+	fmt.Println("Request processed...")
 }
 
 func main() {
@@ -24,13 +28,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print("Server started on 8081")
+	fmt.Println("Server started on 8081")
 
 	for {
+		fmt.Println("waiting for client to connect")
 		conn, err := listner.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
-		do(conn)
+		fmt.Println("Client Connected")
+		go do(conn)
 	}
 }
