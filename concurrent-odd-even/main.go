290227c1768c375741f7b436da4bc1907822b682
Vivek Ajbe <vajbe.tce@gmail.com>
2025-03-16 09:07:40 +0530

    fair threaded stuf

--- concurrent-odd-even/main.go
+++ concurrent-odd-even/main.go
@@ -1,46 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
-const MAX_NUM = 89237849009
+const MAX_NUM int64 = 89237849009
 
-var EvenCount int
+var CurrentNum int64
+var EvenCount int64 // Changed to int64 for consistency with MAX_NUM
 
-func IsEven(num int) bool {
-	return num%2 == 0
+func IsEven(num int64) bool {
+	if num&1 == 0 {
+		return true
+	}
+	return false
 }
 
-func Task(start int, end int, wg *sync.WaitGroup, ch chan int) {
+func Task(start int64, step int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	totalEven := 0
-	for i := start; i <= end; i++ {
-		if IsEven(i) {
-			totalEven += 1
+	localCount := 0
+	for x := start; x < MAX_NUM; x += int64(step) {
+		if IsEven(x) {
+			localCount += 1
 		}
 	}
-	ch <- totalEven
+	atomic.AddInt64(&EvenCount, int64(localCount))
 }
 
 func main() {
-	/*
-		PART 1
-		ch := make(chan int)
-		wg := sync.WaitGroup{}
-		timeNow := time.Now()
-		wg.Add(1)
-		go Task(1, MAX_NUM/2, &wg, ch)
-		wg.Add(1)
-		go Task(MAX_NUM/2+1, MAX_NUM, &wg, ch)
-		b, c := <-ch, <-ch
-		wg.Wait()
-		fmt.Printf("\nTime Elapsed: %fs", time.Now().Sub(timeNow).Seconds())
-		fmt.Printf("\nTotal Even %d, Total Odd %d", b+c, MAX_NUM-(b+c)) */
+	startTime := time.Now()
 
-	ch := make(chan int)
+	numWorkers := 2
 	wg := sync.WaitGroup{}
-	timeN := time.Now()
 
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go Task(int64(i), numWorkers, &wg)
+	}
+
+	wg.Wait()
+
+	fmt.Printf("\nTime Elapsed: %fs", time.Since(startTime).Seconds())
+	fmt.Printf("\nTotal Even: %d, Total Odd: %d\n", EvenCount, MAX_NUM-EvenCount)
 }
