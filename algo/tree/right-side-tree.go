ff696383f1e0347fa4c6e8431760df213aa590da
Vivek Ajbe <vajbe.tce@gmail.com>
2025-06-14 18:28:32 +0530

    right side tree

---
+++ algo/tree/right-side-tree.go
@@ -0,0 +1,64 @@
+// Binary Tree Right Side View
+// LeetCode: https://leetcode.com/problems/binary-tree-right-side-view/
+// Given the root of a binary tree, imagine yourself standing on the right side of it,
+// return the values of the nodes you can see ordered from top to bottom.
+//
+// Example:
+// Input: [1,2,3,null,5,null,4]
+// Output: [1,3,4]
+//
+// The function rightSideView traverses the tree level by level (BFS) and collects the last node's
+// value at each level,
+// which represents the rightmost node visible from the right side.
+
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rightSideView(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+
+	var result []int
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		levelSize := len(queue)
+
+		for i := 0; i < levelSize; i++ {
+			node := queue[0]
+			queue = queue[1:]
+
+			if i == levelSize-1 {
+				result = append(result, node.Val)
+			}
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+
+	return result
+}
+
+func RightSideView() {
+	root := &TreeNode{Val: 1}
+	root.Left = &TreeNode{Val: 2}
+	root.Right = &TreeNode{Val: 3}
+	root.Left.Right = &TreeNode{Val: 5}
+	root.Right.Right = &TreeNode{Val: 4}
+
+	fmt.Println(rightSideView(root)) // Output: [1, 3, 4]
+}
