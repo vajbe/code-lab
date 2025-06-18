7c7936f753a9af94a8cefcb5b9c347d7b3e6413f
Vivek Ajbe <vajbe.tce@gmail.com>
2025-06-18 10:03:55 +0530

    update descroiption

--- algo/binary-search-tree/problems/kth-small-elem.go
+++ algo/binary-search-tree/problems/kth-small-elem.go
@@ -2,8 +2,10 @@
 // Source: https://leetcode.com/problems/kth-smallest-element-in-a-bst/?envType=study-plan-v2&envId=top-interview-150
 //
 // Description:
-// Given the root of a binary search tree and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
-// The solution uses an iterative inorder traversal (with a stack) to collect node values in sorted order.
+// Given the root of a binary search tree and an integer k, return the kth smallest
+// value (1-indexed) of all the values of the nodes in the tree.
+// The solution uses an iterative inorder traversal (with a stack) to collect node
+// values in sorted order.
 //
 // Example BST:
 //      3
